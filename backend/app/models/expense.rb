0c6fe68659234992a45c98f0833f0df66884c44b
shashank99928 <shashank99928@gmail.com>
2026-07-28 08:18:42 +0530

    feat: prevent future dates in expense forms and update backend Docker configuration

--- backend/app/models/expense.rb
+++ backend/app/models/expense.rb
@@ -1,3 +1,13 @@
 class Expense < ApplicationRecord
   belongs_to :category
+
+  validate :date_cannot_be_in_the_future
+
+  private
+
+  def date_cannot_be_in_the_future
+    return if date.blank?
+
+    errors.add(:date, "can't be in the future") if date > Date.current
+  end
 end
